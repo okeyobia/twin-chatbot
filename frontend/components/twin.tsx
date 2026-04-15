17d6bb5a4deb01bb5eb763790385a1600d6985d8
Okechukwu Obiajunwa <okey.obia@gmail.com>
2026-04-15 14:20:48 -0500

    feat: add Lambda deployment script and update fetch URL in Twin component

--- frontend/components/twin.tsx
+++ frontend/components/twin.tsx
@@ -40,7 +40,7 @@ export default function Twin() {
         setIsLoading(true);
 
         try {
-            const response = await fetch('http://localhost:8000/chat', {
+            const response = await fetch('https://y2gyo4of0b.execute-api.us-east-2.amazonaws.com/chat', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
